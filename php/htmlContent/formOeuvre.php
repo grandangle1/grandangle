82106fd6232abcfd868dab142ee3297548858699
skulink <jflavien.dev@gmail.com>
2018-02-14 10:57:09 +0100

    stylisation du formulaire ajout d'oeuvre

--- php/htmlContent/formOeuvre.php
+++ php/htmlContent/formOeuvre.php
@@ -17,20 +17,28 @@
 		<li class="list-group-item">
 			<form class="form-oeuvre">
 				<ul>
-					<li class="list-group"><label>Nom de l'oeuvre</label></li>
+					<li class="list-group bold"><label>Nom de l'oeuvre</label></li>
 					<li class="list-group"><input type="text" name="nomOeuvre"></li><br>
 					<li class="list-group bold"><label>Emplacement de l'exposition :</label></li>
-					<li class="list-group">
-						<input type="radio" name="place" value="1" radio="1">Salle 1
-						<input type="radio" name="place" value="2" radio="2">Salle 2
-						<input type="radio" name="place" value="3" radio="3">Hall principal
-						<input type="radio" name="place" value="4" radio="4">Couloir
+					<li class="list-inline">
+						<div class="list-inline">
+						<input type="radio" name="place" value="1" radio="1" class="list-inline-item" id="salle1"><label for="salle1">Salle 1</label>
+						</div>
+						<div class="list-inline">
+						<input type="radio" name="place" value="2" radio="2" class="list-inline-item" id="salle2"><label for="salle2">Salle 2</label>
+						</div>
+						<div class="list-inline">
+						<input type="radio" name="place" value="3" radio="3" class="list-inline-item" id="hallprinc"><label for="hallprinc">Hall principal</label>
+						</div>
+						<div class="list-inline">
+						<input type="radio" name="place" value="4" radio="4" class="list-inline-item" id="Couloir"><label for="Couloir">Couloir</label>
+					</div>
 					</li><br>
-					<li class="list-group bold"><label>Description de l'oeuvre Fr</label></li>
+					<li class="list-group bold"><label>Description de l'oeuvre en Français</label></li>
 					<li class="list-group"><textarea  name="descrOeuvreFr" rows="5" cols="50"></textarea></li>
 					<li class="list-group bold"><label>Description de l'oeuvre En</label></li><br>
 					<li class="list-group"><textarea  name="descrOeuvreEn" rows="5" cols="50"></textarea></li>
-					<li class="list-group"><button>Envoyer</button></li>
+					<li class="list-group"><button class="btn btn-dark mt-4">Envoyer</button></li>
 				</ul>
 			</form>
 		</li>
