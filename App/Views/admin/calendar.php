585e0d0bdf7bcf3716e41a01b8f300e7f9e7d19c
skulink <jflavien.dev@gmail.com>
2018-02-24 15:04:16 +0100

    maj avec new calendar admin , attention , onglet Actuellement cassé

--- App/Views/admin/calendar.php
+++ App/Views/admin/calendar.php
@@ -7,10 +7,10 @@
     </div>
     <div class="row">
         <div class="col-lg-12">
-            <img src="img/admin/precedent.png" class="pagination float-left" dir="-1">
-            <p class="float-left p-2 font-weight-bold">Précédent</p>
-            <img src="img/admin/suivant.png" class="pagination float-right" dir="1">
-            <p class="float-right p-2 font-weight-bold">Suivant</p>
+            <img src="img/admin/precedent.png" class="pagination float-left salle" dir="-1">
+            <p class="float-left p-2 font-weight-bold prenext">Précédent</p>
+            <img src="img/admin/suivant.png" class="pagination float-right salle" dir="1">
+            <p class="float-right p-2 font-weight-bold prenext">Suivant</p>
         </div>
     </div>
     <div class="row mt-4">
@@ -21,16 +21,16 @@
                     <p class="info card-title">Vous pouvez contacter le gerant Mr/Mme <span class="contact"></span> au <span class="contact"></span> ou par mail : <span class="contact"></span></p>
                     <p class="info card-text">Cet(te) artiste expose <span class="nbOeuvre"></span> oeuvres</p>
                     <div class="text-center">
-                        <p class="newExpo"><img src="img/admin/ajoutExpo.png" class="pr-2">Ajouter une expo</p>
+                        <p class="newExpo"><img src="img/admin/ajoutExpo.png" class="salle pr-2">Ajouter une expo</p>
                     </div>
                     <div class="exist">
                         <div class="float-left">
-                            <div class="p-2"><img src="img/admin/ajouterOeuvre.png" class="addOeuvre pr-2" spec="link" cell="0">Ajouter une oeuvre</div>
-                            <div class="p-2"><img src="img/admin/modifOeuvre.png" class="listOeuvre pr-2" spec="link" cell="0">Modifier une oeuvre</div>
+                            <div class="p-2"><img src="img/admin/ajouterOeuvre.png" class="addOeuvre pr-2 salle" spec="link" cell="0">Ajouter une oeuvre</div>
+                            <div class="p-2"><img src="img/admin/modifOeuvre.png" class="listOeuvre pr-2 salle" spec="link" cell="0">Modifier une oeuvre</div>
                         </div>
                         <div class="float-left">
-                            <div class="p-2"><img src="img/admin/modifExpo.png" class="editExpo pr-2" spec="link" cell="0">Modifier l'exposition</div>
-                            <div class="p-2"><img src="img/admin/supprExpo" class="deleteExpo pr-2" cell="0">Supprimer l'exposition</div>
+                            <div class="p-2"><img src="img/admin/modifExpo.png" class="editExpo pr-2 salle" spec="link" cell="0">Modifier l'exposition</div>
+                            <div class="p-2"><img src="img/admin/supprExpo" class="deleteExpo pr-2 salle" cell="0">Supprimer l'exposition</div>
                         </div>
                     </div>
                 </div>
@@ -43,16 +43,16 @@
                     <p class="info card-title">Vous pouvez contacter le gerant Mr/Mme <span class="contact"></span> au <span class="contact"></span> ou par mail : <span class="contact"></span></p>
                     <p class="info card-text">Cet(te) artiste expose <span class="nbOeuvre"></span> oeuvres</p>
                     <div class="text-center">
-                        <p class="newExpo"><img src="img/admin/ajoutExpo.png" class="pr-2">Ajouter une expo</p>
+                        <p class="newExpo"><img src="img/admin/ajoutExpo.png" class="pr-2 salle">Ajouter une expo</p>
                     </div>
                     <div class="exist">
                         <div class="float-left">
-                            <div class="p-2"><img src="img/admin/ajouterOeuvre.png" class="addOeuvre pr-2" spec="link" cell="1">Ajouter une oeuvre</div>
-                            <div class="p-2"><img src="img/admin/modifOeuvre.png" class="listOeuvre pr-2" spec="link" cell="1">Modifier une oeuvre</div>
+                            <div class="p-2"><img src="img/admin/ajouterOeuvre.png" class="addOeuvre pr-2 salle" spec="link" cell="1">Ajouter une oeuvre</div>
+                            <div class="p-2"><img src="img/admin/modifOeuvre.png" class="listOeuvre pr-2 salle" spec="link" cell="1">Modifier une oeuvre</div>
                         </div>
                         <div class="float-left">
-                            <div class="p-2"><img src="img/admin/modifExpo.png" class="editExpo pr-2" spec="link" cell="1">Modifier l'exposition</div>
-                            <div class="p-2"><img src="img/admin/supprExpo" class="deleteExpo pr-2" cell="1">Supprimer l'exposition</div>
+                            <div class="p-2"><img src="img/admin/modifExpo.png" class="editExpo pr-2 salle" spec="link" cell="1">Modifier l'exposition</div>
+                            <div class="p-2"><img src="img/admin/supprExpo" class="deleteExpo pr-2 salle" cell="1">Supprimer l'exposition</div>
                         </div>
                     </div>
                 </div>
@@ -67,16 +67,16 @@
                     <p class="info card-title">Vous pouvez contacter le gerant Mr/Mme <span class="contact"></span> au <span class="contact"></span> ou par mail : <span class="contact"></span></p>
                     <p class="info card-text">Cet(te) artiste expose <span class="nbOeuvre"></span> oeuvres</p>
                     <div class="text-center">
-                        <p class="newExpo"><img src="img/admin/ajoutExpo.png" class="pr-2">Ajouter une expo</p>
+                        <p class="newExpo"><img src="img/admin/ajoutExpo.png" class="pr-2 salle">Ajouter une expo</p>
                     </div>
                     <div class="exist">
                         <div class="float-left">
-                            <div class="p-2"><img src="img/admin/ajouterOeuvre.png" class="addOeuvre pr-2" spec="link" cell="2">Ajouter une oeuvre</div>
-                            <div class="p-2"><img src="img/admin/modifOeuvre.png" class="listOeuvre pr-2" spec="link" cell="2">Modifier une oeuvre</div>
+                            <div class="p-2"><img src="img/admin/ajouterOeuvre.png" class="addOeuvre pr-2 salle" spec="link" cell="2">Ajouter une oeuvre</div>
+                            <div class="p-2"><img src="img/admin/modifOeuvre.png" class="listOeuvre pr-2 salle" spec="link" cell="2">Modifier une oeuvre</div>
                         </div>
                         <div class="float-left">
-                            <div class="p-2"><img src="img/admin/modifExpo.png" class="editExpo pr-2" spec="link" cell="2">Modifier l'exposition</div>
-                            <div class="p-2"><img src="img/admin/supprExpo" class="deleteExpo pr-2" cell="2">Supprimer l'exposition</div>
+                            <div class="p-2"><img src="img/admin/modifExpo.png" class="editExpo pr-2 salle" spec="link" cell="2">Modifier l'exposition</div>
+                            <div class="p-2"><img src="img/admin/supprExpo" class="deleteExpo pr-2 salle" cell="2">Supprimer l'exposition</div>
                         </div>
                     </div>
                 </div>
@@ -89,16 +89,16 @@
                     <p class="info card-title">Vous pouvez contacter le gerant Mr/Mme <span class="contact"></span> au <span class="contact"></span> ou par mail : <span class="contact"></span></p>
                     <p class="info card-text">Cet(te) artiste expose <span class="nbOeuvre"></span> oeuvres</p>
                     <div class="text-center">
-                        <p class="newExpo"><img src="img/admin/ajoutExpo.png" class="pr-2">Ajouter une expo</p>
+                        <p class="newExpo"><img src="img/admin/ajoutExpo.png" class="pr-2 salle">Ajouter une expo</p>
                     </div>
                     <div class="exist">
                         <div class="float-left">
-                            <div class="p-2"><img src="img/admin/ajouterOeuvre.png" class="addOeuvre pr-2" spec="link" cell="3">Ajouter une oeuvre</div>
-                            <div class="p-2"><img src="img/admin/modifOeuvre.png" class="listOeuvre pr-2" spec="link" cell="3">Modifier une oeuvre</div>
+                            <div class="p-2"><img src="img/admin/ajouterOeuvre.png" class="addOeuvre pr-2 salle" spec="link" cell="3">Ajouter une oeuvre</div>
+                            <div class="p-2"><img src="img/admin/modifOeuvre.png" class="listOeuvre pr-2 salle" spec="link" cell="3">Modifier une oeuvre</div>
                         </div>
                         <div class="float-left">
-                            <div class="p-2"><img src="img/admin/modifExpo.png" class="editExpo pr-2" spec="link" cell="3">Modifier l'exposition</div>
-                            <div class="p-2"><img src="img/admin/supprExpo" class="deleteExpo pr-2" cell="3">Supprimer l'exposition</div>
+                            <div class="p-2"><img src="img/admin/modifExpo.png" class="editExpo pr-2 salle" spec="link" cell="3">Modifier l'exposition</div>
+                            <div class="p-2"><img src="img/admin/supprExpo" class="deleteExpo pr-2 salle" cell="3">Supprimer l'exposition</div>
                         </div>
                     </div>
                 </div>
@@ -107,7 +107,7 @@
     </div>
     <div class="row">
         <div class="col-lg-12 text-center">
-            <div class="mt-5"><img src="img/admin/refresh32.png" class="now pr-2">Revenir à la date du jour</div>
+            <div class="m-5"><img src="img/admin/refresh32.png" class="now pr-2 salle">Revenir à la date du jour</div>
         </div>
     </div>
 </div>
