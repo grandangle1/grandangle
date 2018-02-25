0da71ce3a8a35267fe1b3afc4ae179622832db8c
skulink <jflavien.dev@gmail.com>
2018-02-25 11:19:38 +0100

    maj de la homepage

--- App/Views/guest/en/index.php
+++ App/Views/guest/en/index.php
@@ -1,49 +1,42 @@
-<div class="container mt-5">
+<div class="container-fluid">
     <div class="row">
-
-        <!--  <div class="container">-->
-        <div class="col-lg-8 px">
-            <div class="card img-content">
-                <img src="img/home/home.jpg" class="card-img-top imgaccueil">
-            </div>
+        <div class="col-lg-12">
+            <img src="img/home/homePresent2.jpg" class="home">
         </div>
-
-        <div class="col-lg-4">
-            <div class="card bg-dark text-light text-center ">
-                <div class="card-body">
-                    <h3>Currently on display<br> &amp; and  coming soon</h3>
-                    <p>Hannibal lived during a period of great tension in the Mediterranean Basin, when the Roman Republic established its supremacy over other great powers such as ancient Carthage and the Greek kingdoms of Macedonia, Syracuse, and the Seleucid Empire. One of his most famous achievements was at the outbreak of the Second Punic War, when he marched an army which included war elephants from Iberia over the Pyrenees and the Alps into Italy
-                    </p>
-                </div>
-
-            </div>
-            <br/>
-
-
-            <div class="card bg-dark text-light text-center mb-2 ">
-                <div class="card-body">
-                    <h3>Informations</h3>
-                    <p class="center">Due to these brilliant tactics, Hannibal managed to surround and destroy all but a small remnant of his enemy, despite his own inferior numbers. Depending upon the source, it is estimated that 50,000–70,000 Romans were killed or captured.[3] Among the dead were Roman Consul Lucius Aemilius Paullus, as well as two consuls for the preceding year, two quaestors, twenty-nine out of the forty-eight military tribunes, and an additional eighty senators (at a time when the Roman Senate was composed of no more than 300 men, this constituted 25%–30% of the governing body).
-                    </p>
-                </div>
-
-            </div>
-        </div>
-        <!--  </div>-->
-        <br/>
     </div>
     <div class="row">
-        <div class="col-lg-12 text-center text-light mt-3 mg-3">
-            <div class="card bg-dark">
-                <div class="card-body bg-dark">
-                    <h3>10 reasons for visiting our museum! </h3>
-                    <p>
-                        Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression
+        <div class="col-lg-4 mt-1">
+            <div class="card text-light text-center ht pt-1">
+                <div class="card-body">
+                    <h3 class="pb-3">Information</h3>
+                    <p class="text-center pb-1">18 Place François Sicard 37000 Tours <br>
+                        Phone number : 02 46 56 47 52</p>
+                    <p class="text-center"><span class="bold">Open from tuesday to sunday</span> from 10h to 18h (last entry 17h15)<br>
+                        <span class="bold">Open thursday's nights</span> until 10 pm for temporary expositions (last entry 21h15)<br>
+                        <span class="bold">Free access </span>for permanent collections.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mt-1">
+            <div class="card text-center text-light ht pt-1">
+                <div class="card-body">
+                    <h3 class="pb-5">Grand Angle on a year, it's: </h3>
+                    <div class="float-left ml-2 mr-4"><p class="font-italic"><span class="fsize">22 500</span><br>visits</p></div>
+                    <div class="float-left ml-5"><p class="font-italic"><span class="fsize">44</span><br>artists</p></div>
+                    <div class="float-right mr-2 ml-4"><p class="font-italic"><span class="fsize">570</span><br>works</p></div>
+                    <div class="float-right mr-5"><p class="font-italic"><span class="fsize">12917</span><br> sq ft of exposure</p></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mt-1">
+            <div class="card text-light text-center ht pt-1">
+                <div class="card-body">
+                    <h3 class="pb-5">Our expositions</h3>
+                    <p>Thematic collections, diverse and exciting exhibitions, the Grand Angle Museum is the Center's region's Museum which exhibits the most work in the year.
+                        A force that takes on an even more significant dimension when it is accessible, shared and appropriate by all.
                     </p>
                 </div>
-
             </div>
         </div>
     </div>
-
 </div>
