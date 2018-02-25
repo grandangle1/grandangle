d73ff40bb089a795e2129dbebdfbeae0db0a7121
skulink <jflavien.dev@gmail.com>
2018-02-25 12:13:55 +0100

    finalisation du burger navabar

--- App/Views/template/en/default.php
+++ App/Views/template/en/default.php
@@ -29,7 +29,7 @@
 
 <div class="container-fluid">
     <nav class="navbar navbar-expand-lg navbar-light bg-navbar drop-shadow">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
